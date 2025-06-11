b9535d554ab03ea525a0d1d0755f1fb952874695
samrasugu <mokuasamr@gmail.com>
2025-06-11 09:18:51 +0300

    About section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,15 +4,15 @@ import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <section className='hero'>
         <img src={profile_img} alt="" />
         <h1><span>I'm Alex Bennet,</span> frontend developer based in USA.</h1>
-        <p>I’m passionate and detail-oriented web developer with a strong focus on creating beautiful, functional, and user-friendly digital experiences. I specialize in front-end development, blending creativity with code to bring ideas to life. With a solid foundation in HTML, CSS, JavaScript, and modern frameworks.</p>
+        <p>I'm passionate and detail-oriented web developer with a strong focus on creating beautiful, functional, and user-friendly digital experiences. I specialize in front-end development, blending creativity with code to bring ideas to life. With a solid foundation in HTML, CSS, JavaScript, and modern frameworks.</p>
         <div className="hero-action">
             <div className="hero-connect">Connect with me</div>
             <div className="hero-resume">My resume</div>
         </div>
-    </div>
+    </section>
   )
 }
 
